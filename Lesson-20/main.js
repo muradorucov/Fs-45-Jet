09aa5dfd5f3e4591ccdc7b4412de6d2757ae16b3
muradorucov <murad.po@code.edu.az>
2026-03-11 12:59:54 +0400

    scss

---
+++ Lesson-20/main.js
@@ -0,0 +1,80 @@
+// console.log("Test");//test
+// console.log("Test");//test
+
+// throw new Error("Error");
+
+// console.log("Test");
+// console.log("Test");
+// console.log("Test");
+// console.log("Test");
+// console.log("Test");
+// console.log("Test");
+
+
+
+// string
+// Number
+// Boolean
+
+// let age = 45;
+// age = "Murad"
+
+// C#
+// let age<Number> = 45;
+
+
+// "4" +7= 47
+// "4" - 7= -3
+
+
+
+// Ts
+
+// js
+
+
+// C#
+
+// Pyhton
+// let var const
+// age = 67
+// age = 45
+// console.log("Murad");
+// document.writeln("Murad")
+
+
+// react, next, vue
+
+
+// document.writeln("Test edirem")
+// console.error("error oldu")
+// console.log("Murad");
+
+// console.info()
+// console.warn()
+
+// alert("Js is coming....")
+// let username = prompt("Adinizi daxil edin....", "Murad")
+// console.log(username);
+// console.log(username.toLowerCase());
+// console.log(username.toUpperCase());
+
+// let val = confirm("Blogu silmek isetyirsinizmi?")
+// console.log(val);
+
+// throw new Error("Xeta olduuuuu");
+
+// console.log("test 3");
+
+
+// Output & Input
+// Data types
+// Variables
+// Operators
+// op: conditions
+// op: Loops
+// function
+// array
+// Objects
+
+
